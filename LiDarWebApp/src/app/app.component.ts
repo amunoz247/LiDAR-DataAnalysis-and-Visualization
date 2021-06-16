97e79d5c50b5d6195bfcbd6a7f22b77d32d38b3f
Andrew E Munoz <amunoz24@nevada.unr.edu>
2021-06-16 11:39:48 -0700

    Updated Main Web Page to check output data in table

--- LiDarWebApp/src/app/app.component.ts
+++ LiDarWebApp/src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { MqttSocketService } from './mqtt/mqttsocket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'LiDarWebApp';
+  public pointCloud : PCD[] = [];
+  parsedJSON: any;
+
+  constructor(private ms : MqttSocketService){}
+
+  // Table Headers to be displayed on Webpage
+  headers = ["time", "topic", "x", "y", "z", "intensity"]
+
+
+  ngOnInit()
+  {
+    //Scope variable to access point cloud class array
+    var app = this;
+
+    this.ms.on('mqtt_message', function(value){
+        console.log(value);
+        //console.log(pcd);
+        console.log(this);
+        console.log(value.payload);
+        this.parsedJSON = JSON.parse(value.payload);
+        console.log(this.parsedJSON);
+        if (app.pointCloud.length > 10){
+          var val = app.pointCloud.pop();
+          console.log(val);
+        }
+        app.pointCloud.push({time: value.time, topic: value.topic, x: this.parsedJSON.x, 
+          y: this.parsedJSON.y, z: this.parsedJSON.z, intensity: this.parsedJSON.intensity});
+    });
+
+
+  }
+}
+
+// Point Cloud class to handle data from MQTT messages
+export class PCD{
+  time:string;
+  topic:string;
+
+  //Payload point cloud values
+  x:[];
+  y:[];
+  z:[];
+  intensity:[];
 }
