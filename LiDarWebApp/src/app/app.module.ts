ca04fe9bfb402cf3e8b0c9a6b9dfb1512f20b0a3
Andrew E Munoz <amunoz24@nevada.unr.edu>
2021-06-24 16:11:46 -0700

    UI: Created header and footer

--- LiDarWebApp/src/app/app.module.ts
+++ LiDarWebApp/src/app/app.module.ts
@@ -4,18 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
 import { MqttSocketService } from './mqtt/mqttsocket.service';
 import { RendererComponent } from './renderer/renderer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RendererComponent
+    RendererComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MdbCheckboxModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule, 
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [MqttSocketService],
   bootstrap: [AppComponent]
