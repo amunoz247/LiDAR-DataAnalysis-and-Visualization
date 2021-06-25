f4c69b614b8b622cdf5ad6e780405cc04675bcb2
Andrew E Munoz <amunoz24@nevada.unr.edu>
2021-06-25 16:05:50 -0700

    Created Home Page and added background image

--- LiDarWebApp/src/app/app.module.ts
+++ LiDarWebApp/src/app/app.module.ts
@@ -6,22 +6,24 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MdbCheckboxModule } from 'mdb-angular-ui-kit/checkbox';
 import { MqttSocketService } from './mqtt/mqttsocket.service';
+import { HeaderComponent } from './ui_components/header/header.component';
+import { FooterComponent } from './ui_components/footer/footer.component';
 import { RendererComponent } from './renderer/renderer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './ui_components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RendererComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RendererComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
